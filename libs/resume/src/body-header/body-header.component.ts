c27a714e6d65296aba025d494e66e8030a6e5842
RaviTejaVattem <ravitejavattem@gmail.com>
2021-11-01 20:40:02 +0530

    Added dynamic experience calculation

--- libs/resume/src/body-header/body-header.component.ts
+++ libs/resume/src/body-header/body-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'body-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyHeaderComponent implements OnInit {
 
-  constructor() { }
+  totalExperience = '';
 
-  ngOnInit() {
+  constructor() {
+    const joiningDate = dayjs('2015-01-26');
+    const today = dayjs();
+    const monthsOfExperience: number = today.diff(joiningDate, 'month');
+    const years = Math.floor(monthsOfExperience / 12);
+    const months = monthsOfExperience % 12;
+
+    this.totalExperience = months ? `${years}.${months}` : years.toString();
   }
+
+  ngOnInit() { }
 
 }
